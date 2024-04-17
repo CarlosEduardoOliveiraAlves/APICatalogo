18a75e23e8fd5c1195099a5dc7f1160a77981d6c
Carlos Eduardo Oliveira Alves <carlos.eduardo.oliver.alves@gmail.com>
2024-04-17 18:04:33 -0300

    Feat - criação de arquivo de contexto

---
+++ API/Context/ApiCatalogoContext.cs
@@ -0,0 +1,15 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Context;
+
+public class ApiCatalogoContext : DbContext
+{
+    public ApiCatalogoContext(DbContextOptions<ApiCatalogoContext> options ) : base( options )
+    {
+            
+    }
+
+    public DbSet<Categoria>? Categorias { get; set; }
+    public DbSet<Produto>? Produtos { get; set; }
+}
