42b0fdf6058451799c5576ca5e92ef7671e26137
Carlos Eduardo Oliveira Alves <carlos.eduardo.oliver.alves@gmail.com>
2024-04-17 18:05:33 -0300

    Feat - Criação dos Models

---
+++ API/Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Models;
+
+// Classe Anêmica (Sem comportamento)
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public String? Nome { get; set; } //? usado para que o campo possa ser null
+    public string? ImagemUrl { get; set; } //? usado para que o campo possa ser null
+}
---
+++ API/Models/Produto.cs
@@ -0,0 +1,19 @@
+﻿namespace API.Models;
+
+// Classe Anêmica (Sem comportamento)
+public class Produto
+{
+    public int ProdutoId { get; set; }
+
+    public string? Nome { get; set; }
+
+    public string? Descricao { get; set; }
+
+    public decimal Preco { get; set; }
+
+    public string? ImagemUrl { get; set;}
+
+    public float Estoque { get; set; }
+
+    public DateTime DataCadastro { get; set; }
+}
