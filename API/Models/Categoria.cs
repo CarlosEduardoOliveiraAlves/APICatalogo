141db3566cc5f8ea085f89f2c47947102a3f35ca
Carlos Eduardo Oliveira Alves <carlos.eduardo.oliver.alves@gmail.com>
2024-04-18 15:20:54 -0300

    Feat - Aplicando Data Annotations nas entidades

--- API/Models/Categoria.cs
+++ API/Models/Categoria.cs
@@ -1,9 +1,28 @@
-﻿namespace API.Models;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models;
 
 // Classe Anêmica (Sem comportamento)
+[Table("Categorias")]
 public class Categoria
 {
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+
+    [Key]
     public int CategoriaId { get; set; }
-    public String? Nome { get; set; } //? usado para que o campo possa ser null
+
+    [Required]
+    [StringLength(80)]
+    public string? Nome { get; set; } //? usado para que o campo possa ser null
+    
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; } //? usado para que o campo possa ser null
+
+    public ICollection<Produto>? Produtos { get; set; }
 }
